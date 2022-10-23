d57d02d5ba9fd4c71b9fed7441ff9003bf9773d8
Brian Davidson <116458299+brian-a-davidson@users.noreply.github.com>
2022-10-23 12:31:48 -0500

    Update email address

--- gs_file_check.sh
+++ gs_file_check.sh
@@ -11,12 +11,12 @@ file_list_name_yt=/opt/monitor_scripts/config_files/gs_file_list_yesterday.txt
 service_message=/opt/monitor_scripts/config_files/service_desk_message.txt
 
 # Local Email Variables
-sender_email="brian.allan.davidson@gmail.com"
-email_list="brian.allan.davidson@gmail.com"
+sender_email="test-email@gmail.com"
+email_list="test-email@gmail.com"
 
 # Local Fail Email Variables
-fail_sender="brian.allan.davidson@gmail.com"
-fail_email_list="brian.allan.davidson@gmail.com"
+fail_sender="test-email@gmail.com"
+fail_email_list="test-email@gmail.com"
 
 # Get Remote Variables
 filedate=$(sed -n 's/DATE=//p' $config_file) 
@@ -25,14 +25,14 @@ filecount=$(sed -n 's/FILECOUNT=//p' $config_file)
 clear="0"
 
 if [[ "$filecount" -ge 3 ]]; then
-        subject="SUCCESS - ALL GlobeSherpa Sales files received for $filedate - File Count:$filecount"
+        subject="SUCCESS - ALL GS Sales files received for $filedate - File Count:$filecount"
         cat $file_list_name | mail -r $sender_email -s "$subject" $email_list
         cat $file_list_name > $file_list_name_yt
         sed -i "s/FILECOUNT=$filecount/FILECOUNT=$clear/g" $config_file
         sed -i "s/EMAIL=$fileemail/EMAIL=$clear/g" $config_file
         sed -i "s/DATE=$filedate/DATE=$clear/g" $config_file
 else
-        subject="P1: FAILED - NOT ALL 3 GlobeSherpa Sales files received for $filedate - File Count:$filecount"
+        subject="P1: FAILED - NOT ALL 3 GS Sales files received for $filedate - File Count:$filecount"
 	cat $file_list_name >> $service_message
         cat $file_list_name_yt >> $service_message
 	cat $service_message | mail -r $fail_sender -s "$subject" $fail_email_list
